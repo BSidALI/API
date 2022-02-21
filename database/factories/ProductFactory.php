0e3c080a93a37ccfd719a7b9faec1a97a766065c
BSidALI <ali242011@hotmail.com>
2022-02-21 22:33:56 +0100

    facorue & migraion

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,8 +4,11 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+use Faker\Generator as Faker;
+
 class ProductFactory extends Factory
 {
+    
     /**
      * Define the model's default state.
      *
@@ -13,7 +16,12 @@ class ProductFactory extends Factory
      */
     public function definition()
     {
-        return [
+        return ['name'=>$faker->word,
+                'detail'=>$faker->paragraph,
+                'price'=>$faker->numberBetween(100,200),
+                'stock'=>$faker->randomDigit,
+                'discount'=>$faker->numberBetween(2,30),
+
             //
         ];
     }
--- database/factories/ReviewsFactory.php
+++ database/factories/ReviewsFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
 
 class ReviewsFactory extends Factory
 {
@@ -14,7 +15,12 @@ class ReviewsFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'product_id'=>function(){
+                return Product::all()->random(); // product her is model
+            },
+            'customer'=>$faker->name,
+            'review'=>$faker->paragraph,
+            'star'=>$faker->numberBetween(100,200),
         ];
     }
 }
