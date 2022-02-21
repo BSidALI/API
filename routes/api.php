151163d56ef5342a80be7c9fe9c2051a51586f46
BSidALI <ali242011@hotmail.com>
2022-02-21 21:22:18 +0100

    creat model -a

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('/Products','ProductController');
+
+Route::group(['prifix'=>'products'],function(){
+
+    Route::apiResource
+    ('{product}/reviews','ReviewsController');
+    });
+    
+    
