aa14461d417d74c460576a996979ed3484d03872
BSidALI <ali242011@hotmail.com>
2022-02-22 12:42:50 +0100

    add rating

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,8 +19,11 @@ class ProductResource extends JsonResource
             'price' => $this->price,
             'stock' =>  $this->stock,
             'discount' =>$this->discount,
-            // get relation that said 1 product -> have reviews in relation
-            'rating'=>$this->reviews->sum('star')/$this->reviews->count(),
+            'totale price '=>round((1-($this->discount)/100)*$this->price),
+            // get relation that said 1 product -> have reviews in relation we do condition with $this->reviews->count()>0?
+            'rating'=>$this->reviews->count()>0?round ($this->reviews->sum('star')/$this->reviews->count())
+            // if connection is no
+            :"noting rating yet",
             // now u can add extra work what will hapen and what this one can acc in same page
             // exp her u can add href link  who will have name and link to generat page link
             // this one is the name u will see in html
